c81845ffce40e39c8e88d89301d258ae2862989e
Константин Александрович Игнатов <ignatov_ka@hotmail.com>
2022-11-21 13:08:12 +1000

    Add project files.

---
+++ MimeFileList/FNameSizeMime.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MimeFileList
+{
+    internal class FNameSizeMime
+    {
+        private string name;
+        private long size;
+        private string mimeMap;
+
+        public string Name { get => name; set => name = value; }
+        public long Size { get => size; set => size = value; }
+        public string MimeMap { get => mimeMap; set => mimeMap = value; }
+    }
+
+    internal static class FNameSizeMimeStat
+    {
+        public static void StatisticToConsole(List<FNameSizeMime> ls)
+        {
+            var countDictionary = new Dictionary<string, int>();
+            var sizeDictionary = new Dictionary<string, long>();
+            int countAll = 0;
+            foreach (FNameSizeMime fname in ls)
+            {
+                Console.WriteLine(Path.GetFileName(fname.Name) + "    " + fname.Size + "    " + fname.MimeMap);
+            }
+
+            foreach (var e in ls)
+            {
+                if (e.MimeMap == null)
+                    continue;
+                if (!countDictionary.ContainsKey(e.MimeMap)) countDictionary[e.MimeMap] = 0;
+                countDictionary[e.MimeMap]++;
+            }
+
+            foreach (var s in countDictionary.Values)
+            {
+                countAll += s;
+            }
+
+            foreach (var ee in ls)
+            {
+                if (ee.MimeMap == null)
+                    continue;
+                if (!sizeDictionary.ContainsKey(ee.MimeMap)) sizeDictionary[ee.MimeMap] = 0;
+                sizeDictionary[ee.MimeMap]+=ee.Size;
+            }
+
+            foreach (var e in countDictionary)
+            {
+                Console.WriteLine(e.Key + "\t" + e.Value + "\t" + string.Format("{0:P1}", (double)(e.Value) / (double)(countAll)) 
+                    + "\t" + (sizeDictionary[e.Key] / e.Value));
+            }
+        }
+
+
+    }
+
+}
---
+++ MimeFileList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Web;
+
+namespace MimeFileList
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //List<FNameSizeMime> list = _GetRecursFiles(Directory.GetCurrentDirectory());
+            List<FNameSizeMime> ls = GetRecursFiles(Directory.GetCurrentDirectory());
+            FNameSizeMimeStat.StatisticToConsole(ls);
+            Console.ReadKey();
+        }
+        private static List<FNameSizeMime> GetRecursFiles(string start_path)
+        {
+            long allSize = 0;
+            List<FNameSizeMime> listMime = new List<FNameSizeMime>();
+            try
+            {
+                string[] folders = Directory.GetDirectories(start_path);
+                listMime.Add(new FNameSizeMime { Name = start_path });
+                foreach (string folder in folders)
+                {
+                    listMime.AddRange(GetRecursFiles(folder));
+                    allSize += listMime.FindLast(n => n.Name == folder).Size;
+                }
+                string[] files = Directory.GetFiles(start_path);
+                foreach (string filename in files)
+                {
+                    FileInfo file = new FileInfo(filename);
+                    long size = file.Length;
+                    listMime.Add(new FNameSizeMime { Name = filename, Size = file.Length, MimeMap = MimeMapping.GetMimeMapping(filename) });
+                    allSize += file.Length;
+                }
+                listMime.FindLast(n=> n.Name == start_path).Size = allSize;
+                
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return listMime;
+        }
+    }
+}
